0e9b1b0275f2f65fe16b0bee8aa22d873b68598f
sglondon <susangs2@gmail.com>
2018-01-29 11:39:52 -0500

    added cat class and attributes

--- Cat.cs
+++ Cat.cs
@@ -2,9 +2,52 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Intro to oop
+namespace IntroToOOP
 {
     class Cat
+{
+    //fields
+    //I need States
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+       
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+
+
+
+    //I need behavoirs
+    //always methods
+    //constructors
+    public Cat()
     {
+        //default constructor
+        //takes no parameters(nothing in parentheses)
     }
+
+    public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+    public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+
+            Console.WriteLine("Is the cat hungry? " + isHungry);
+        }
 }
+}
+
